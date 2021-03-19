69a1f07cd6e583644e0497d6f0d295efb610a47d
SyllaSafion <m.sylla.safion@gmail.com>
2021-03-19 17:07:25 +0100

    Added mongo crud and api

---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,24 @@
+const controllers = require('../controllers/crmControllers');
+
+const routes = (app) => {
+    // route contacts pour lister et ajouter des contacts
+    app.route('/contacts')
+    .get( (req,res,next) => {
+
+        //mon middleware pour traiter les demandes
+        console.log('Request GET of a user from ' + req.originalUrl );
+        next();
+
+    }, controllers.getContacts )
+    // la méthode POST me permet d'ajouter un nouveau contact
+    .post( controllers.addNewContact );
+
+    // route contacts avec param id pour modifier ou supprimer des contacts
+    app.route('/contacts/:id')
+    .get( controllers.getContactByID )
+    .put( controllers.updateContact )
+    .delete( controllers.deleteContact );
+
+};
+
+exports.routes = routes;
